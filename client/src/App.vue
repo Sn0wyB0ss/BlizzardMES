3cccf5eaa7ee6eb627ef1cfcb5fb2157981dde6e
Daniel Soares Alves - Sn0wyB0ss <zenter2018@gmail.com>
2026-09-22 20:34:54 -0300

    Added Routing
    Added Routing to Different Pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,59 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Test from "./components/Test.vue";
+
+const routes = {
+  "/": TheWelcome,
+  "/hello_world": Test,
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"] || HelloWorld;
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="main">
+    <header>
+      <div class="wrapper">
+        <a href="#/">TheWelcome</a>
+        <a href="#/hello_world">Test</a>
+      </div>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <component :is="currentView" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
+.main {
+  position: absolute;
+  left: 50%;
+  top: 0%;
+  transform: translate(-50%, 0%);
+  width: 600px;
+  text-align: center;
+}
+
 header {
+  position: sticky;
+  top: 0;
+  padding: 10px 16px;
   line-height: 1.5;
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
